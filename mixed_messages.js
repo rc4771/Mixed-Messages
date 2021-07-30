b79418bbe29ba39dc1b115150ac645761099ce63
Rafeed Choudhury <rc4771@rit.edu>
2021-07-30 10:29:40 +0600

    Added odds and edited predictions to hold odds

--- mixed_messages.js
+++ mixed_messages.js
@@ -13,23 +13,30 @@ function getMatchDay() {
 function getScore() {
     const homeGoals = Math.floor(Math.random() * 7);
     const awayGoals = Math.floor(Math.random() * 7);
-    return [homeGoals, awayGoals];
+    return `${homeGoals}-${awayGoals}`;
 }
 
-function getPredictions(matchDay, score) {
+function getOdds() {
+    const leftOdd = Math.floor(Math.random() * 11) + 1;
+    const rightOdd = Math.floor(Math.random() * 11) + 1;
+    return `${leftOdd}/${rightOdd}`;
+}
+function getPredictions(matchDay, score, odds) {
     if (score[0] < score[1]) {
-        return `${matchDay[0]} to lose to ${matchDay[1]} ${score[0]}-${score[1]}`;
+        return `${matchDay[0]} to lose to ${matchDay[1]} ${score} at ${odds} odds.`;
     }
     else if (score[0] > score[1]) {
-        return `${matchDay[0]} to beat ${matchDay[1]} ${score[0]}-${score[1]}`;
+        return `${matchDay[0]} to beat ${matchDay[1]} ${score} at ${odds} odds.`;
     }
     else {
-        return `${matchDay[0]} to draw to ${matchDay[1]} ${score[0]}-${score[1]}`;
+        return `${matchDay[0]} to draw to ${matchDay[1]} ${score} at ${odds} odds.`;
     }
 }
 
 const matchDay = getMatchDay();
 const score = getScore();
+const odds = getOdds();
 console.log(matchDay);
 console.log(score);
-console.log(getPredictions(matchDay, score));
+console.log(odds);
+console.log(getPredictions(matchDay, score, odds));
