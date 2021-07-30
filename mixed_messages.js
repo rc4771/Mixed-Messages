33c834c084bb1face9ed585501984c28acfa0756
Rafeed Choudhury <rc4771@rit.edu>
2021-07-30 10:18:47 +0600

    Added predictions and tweaked score to make it more realistic

--- mixed_messages.js
+++ mixed_messages.js
@@ -3,7 +3,7 @@ const teams = ["Arsenal", "Aston Villa", "Brentford", "Brighton", "Burnley", "Ch
 
 function getMatchDay() {
     const home = teams[Math.floor(Math.random() * teams.length)];
-    const away = teams[Math.floor(Math.random() * teams.length)];
+    let away = teams[Math.floor(Math.random() * teams.length)];
     while (home === away) {
         away = teams[Math.floor(Math.random() * teams.length)];
     }
@@ -11,10 +11,25 @@ function getMatchDay() {
 }
 
 function getScore() {
-    const homeGoals = Math.floor(Math.random() * 11);
-    const awayGoals = Math.floor(Math.random() * 11);
+    const homeGoals = Math.floor(Math.random() * 7);
+    const awayGoals = Math.floor(Math.random() * 7);
     return [homeGoals, awayGoals];
 }
 
-console.log(getMatchDay());
-console.log(getScore());
+function getPredictions(matchDay, score) {
+    if (score[0] < score[1]) {
+        return `${matchDay[0]} to lose to ${matchDay[1]} ${score[0]}-${score[1]}`;
+    }
+    else if (score[0] > score[1]) {
+        return `${matchDay[0]} to beat ${matchDay[1]} ${score[0]}-${score[1]}`;
+    }
+    else {
+        return `${matchDay[0]} to draw to ${matchDay[1]} ${score[0]}-${score[1]}`;
+    }
+}
+
+const matchDay = getMatchDay();
+const score = getScore();
+console.log(matchDay);
+console.log(score);
+console.log(getPredictions(matchDay, score));
